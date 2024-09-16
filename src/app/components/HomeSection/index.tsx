87915d5a85b02857d3b404399d43cc08599ecfc6
mtdias96 <matheusdias.front96@gmail.com>
2024-09-15 22:40:09 -0300

    motion-home

--- src/app/components/HomeSection/index.tsx
+++ src/app/components/HomeSection/index.tsx
@@ -1,8 +1,17 @@
+'use client'
+
+import { motion } from 'framer-motion'
+
 export function HomeSection() {
   return (
     <section className="h-[818px] mt-32">
       <div className="flex flex-col items-start md:flex-row">
-        <div className="flex flex-col justify-start gap-5 w-1/2 bg-red-500">
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          className="flex flex-col justify-start gap-5 w-1/2 "
+        >
           <h1 className="text-white text-[5rem] font-bold leading-[4.5rem] tracking-[-1px] max-w-[700px] uppercase">
             Transforme ideias em Resultados Reais
           </h1>
@@ -16,15 +25,21 @@ export function HomeSection() {
           <button className="bg-blue-500 text-white py-2 px-4 rounded max-w-48 mt-12">
             Saiba Mais
           </button>
-        </div>
+        </motion.div>
 
-        <div className="flex justify-center items-center w-1/2">
+        <motion.div
+          className="flex justify-center items-center w-1/2 origin-center"
+          initial={{ opacity: 0, y: 200, scale: 0.5 }}
+          whileInView={{ opacity: 1, y: 0, scale: 1 }}
+          exit={{ opacity: 0, y: 200, scale: 0.5 }}
+          transition={{ duration: 0.5 }}
+        >
           <img
             src="/hero-illustration.svg"
             alt="Programador mexendo no computador"
             className="max-w-full h-auto"
           />
-        </div>
+        </motion.div>
       </div>
     </section>
   )
